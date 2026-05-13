077de5956c52e09fa5b29fb796c4e4dc86486409
daniil.konovalenko1@nure.ua <daniil.konovalenko1@nure.ua>
2026-05-13 19:48:33 +0300

    Revert accidental restore of Identity columns: drop columns and clean temporary migrations; ignore properties in UserContext

--- WebApp/MedicationManagement/DBContext/UserContext.cs
+++ WebApp/MedicationManagement/DBContext/UserContext.cs
@@ -9,5 +9,21 @@ namespace MedicationManagement.DBContext
         public UserContext(DbContextOptions<UserContext> options) : base(options)
         {
         }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Ignore Identity properties that are intentionally removed from the schema
+            builder.Entity<ApplicationUser>(b =>
+            {
+                b.Ignore(u => u.PhoneNumber);
+                b.Ignore(u => u.PhoneNumberConfirmed);
+                b.Ignore(u => u.TwoFactorEnabled);
+                b.Ignore(u => u.LockoutEnd);
+                b.Ignore(u => u.LockoutEnabled);
+                b.Ignore(u => u.AccessFailedCount);
+            });
+        }
     }
 }
--- WebApp/MedicationManagement/Migrations/User/20260513164720_DropIdentityColumns.Designer.cs
+++ WebApp/MedicationManagement/Migrations/User/20260513164720_DropIdentityColumns.Designer.cs
@@ -3,6 +3,7 @@ using MedicationManagement.DBContext;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace MedicationManagement.Migrations.User
 {
     [DbContext(typeof(UserContext))]
-    partial class UserContextModelSnapshot : ModelSnapshot
+    [Migration("20260513164720_DropIdentityColumns")]
+    partial class DropIdentityColumns
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
